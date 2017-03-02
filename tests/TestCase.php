e669a645e4d7acf60dc913c24b7c7201f4990e54
Eduardo Bizarro <edbizarro@gmail.com>
2017-03-02 01:43:45 -0300

    tests

---
+++ tests/TestCase.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Spatie\MediaLibrary\Test;
+
+use Convenia\Revisionable\RevisionableServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+//        $this->setUpDatabase($this->app);
+    }
+
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            RevisionableServiceProvider::class,
+        ];
+    }
+
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $this->initializeDirectory($this->getTempDirectory());
+
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+
+        $app['config']->set('app.key', '6rE9Nz59bGRbeMATftriyQjrpF7DcOQm');
+    }
+}
