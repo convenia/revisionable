f0d27dc11661a801d80add12a2396eef96a11f7e
Eduardo Bizarro <edbizarro@gmail.com>
2017-03-01 22:15:46 -0300

    refactor migrations

---
+++ src/RevisionableServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Convenia\Revisionable;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class RevisionableServiceProvider
+ */
+class RevisionableServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        foreach ($this->getMigrations() as $migration) {
+            $this->publishes([
+                __DIR__.'/../database/migrations/'.$migration.'.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_'.$migration.'.php'),
+            ], 'migrations');
+        }
+    }
+
+    protected function getMigrations()
+    {
+        return [
+            'create_revisions_table',
+        ];
+    }
+}
